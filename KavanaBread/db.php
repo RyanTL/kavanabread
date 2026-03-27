0cc09c318e1a85a15a749a82919515e08ad36ed6
Michiru3261 <aco.kelvin@gmail.com>
2026-03-27 11:32:02 -0400

    Update db.php

--- KavanaBread/db.php
+++ KavanaBread/db.php
@@ -1,5 +1,5 @@
 <?php
-#Estas son las credenciales del usuario de la base de datos
+#Estas son las credenciales del usuario de la base de datos y tambien permite la coneccion de la base de datos con la pagina web
 session_start();
 $servername = "localhost";
 $username = "kavanaAdmin";
@@ -12,7 +12,7 @@ if (mysqli_connect_error())
   {
     die("Database connection failed: " . mysqli_connect_error());
   }
-  #Esta en banco por el momento pero sesupone que hiba conected to the database haci que si no les sale en la pagina se concecto corrctamente
+  #Esta en blanco por el momento pero sesupone que hiba conected to the database haci que si no les sale en la pagina se concecto corrctamente
   echo "";
 
 
